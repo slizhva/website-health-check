373ed5a992bdbbc079b3b9714843a18faaed187d
slizhva <slizhva@gmail.com>
2025-07-14 16:55:00 +0300

    Fix health check command.

--- app/Console/Commands/CheckHealthCommand.php
+++ app/Console/Commands/CheckHealthCommand.php
@@ -8,31 +8,50 @@ use Illuminate\Console\Command;
 use App\Models\Links;
 use App\Events\LinkUnavailableEvent;
 use App\Events\LinkCheckExceptionEvent;
+use Symfony\Component\Console\Command\Command as ConsoleCommand;
 
 class CheckHealthCommand extends Command
 {
     protected $signature = 'health:check';
     protected $description = 'Check websites health';
 
-    public function handle():int {
+    public function handle(): int
+    {
         $links = Links
             ::get(['id', 'user', 'url', 'success_content', 'status']);
 
         foreach ($links as $link) {
             try {
-                $isCheckSuccess = str_contains(file_get_contents($link->url), $link['success_content']);
-                Links::where('id', $link->id)->update([
-                    'status' => $isCheckSuccess ? Links::STATUS_AVAILABLE : Links::STATUS_UNAVAILABLE
+                $curl = curl_init();
+
+                curl_setopt_array($curl, [
+                    CURLOPT_URL => $link['url'],
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 20,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                ]);
+
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+
+                $isCheckSuccess = str_contains($response, $link['success_content']);
+                Links::whereId($link['id'])->update([
+                    'status' => $isCheckSuccess ? Links::STATUS_AVAILABLE : Links::STATUS_UNAVAILABLE,
                 ]);
 
                 LinkUnavailableEvent::dispatchIf(!$isCheckSuccess, $link);
             } catch (Exception) {
-                Links::where('id', $link->id)->update(['status' => Links::STATUS_PENDING]);
+                Links::whereId($link['id'])->update(['status' => Links::STATUS_PENDING]);
 
                 LinkCheckExceptionEvent::dispatch($link);
             }
         }
 
-        return Command::SUCCESS;
+        return ConsoleCommand::SUCCESS;
     }
 }
